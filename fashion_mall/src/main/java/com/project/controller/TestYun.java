9624578047dbb8f0780f803d206b1c9e0ebb27fd
yunseo <redtastes@naver.com>
2022-05-31 17:31:08 +0900

    테스트-윤서-

--- fashion_mall/src/main/java/com/project/controller/TestYun.java
+++ fashion_mall/src/main/java/com/project/controller/TestYun.java
@@ -2,6 +2,6 @@ package com.project.controller;
 
 public class TestYun {
 	public static void main(String[] args) {
-		System.out.println("테스트!!!!!!!!!!  -윤서-");
+		System.out.println("두번째 테스트");
 	}
 }
