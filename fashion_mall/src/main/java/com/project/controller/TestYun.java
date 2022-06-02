a3f00195bc17095a57f72acb6f0c5d652d9d5025
yunseo <redtastes@naver.com>
2022-06-02 10:39:39 +0900

    java 올리기 테스트

--- fashion_mall/src/main/java/com/project/controller/TestYun.java
+++ fashion_mall/src/main/java/com/project/controller/TestYun.java
@@ -4,6 +4,6 @@ public class TestYun {
 	public static void main(String[] args) {
 		System.out.println("두번째 테스트");
 		
-		System.out.println("테스트 그만할래");
+		System.out.println("테스트 그만할래!!!!");
 	}
 }
